22aaec6032d8582c79b80b5337ed4ce0e2d6aece
RÔ TIN <tinro070@gmail.com>
2024-11-02 10:44:18 +0700

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>This is Blog page.....</h1>
+</template>
+
+<script>
+export default {
+    name: "AboutPage",
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>This is Blog page.....</h1>
+</template>
+
+<script>
+export default {
+    name: "BlogPage",
+
+};
+</script>
+
+<style>
+
+</style>
